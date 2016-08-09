e1ca553cd07cb4e124c9b7ba6364ad678040ec78
Philip Wahlbom <philip@wahlbom.net>
2016-08-09 11:55:22 -0500

    Add project files.

---
+++ CrazyMath/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
